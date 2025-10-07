efd468cb2e21e068038805ce8caf658fdb6a1e8a
mijinkim <alwls0808@gmail.com>
2025-10-07 13:37:46 +0900

    init commit

---
+++ src/main/java/com/example/lifeshare/LifeshareApplication.java
@@ -0,0 +1,14 @@
+package com.example.lifeshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeshareApplication.class, args);
+		System.out.println("HELLO~!!!");
+	}
+
+}
---
+++ src/test/java/com/example/lifeshare/LifeshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifeshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
